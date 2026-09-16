76f1f23fa5cb34b520766d0537d374013df8cebf
nancyjurado26 <nancyju2024@gmail.com>
2026-09-16 12:53:02 -0500

    Update db.js
    cambio process.env.MONGODB_URI

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const conectarDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGO_URI);
+        await mongoose.connect(process.env.MONGODB_URI);
 
         console.log('✅ MongoDB conectado');
     } catch (error) {
